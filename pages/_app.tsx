ab447b11d2998ce631f6cd5e558590e5ba94d7b1
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2022-12-26 22:29:47 -0800

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faArrowUp,
+  faCar,
+  faCheckSquare,
+  faChild,
+  faCoffee,
+  faUniversalAccess,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+
+library.add(
+  faCoffee,
+  faCheckSquare,
+  faUser,
+  faChild,
+  faUniversalAccess,
+  faCar,
+  faArrowUp
+);
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+export default MyApp;
