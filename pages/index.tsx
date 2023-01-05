bc4305d142cf6a27ce6179e99ca0f5b740a31810
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2023-01-05 15:40:56 -0800

    added google font

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,7 @@ const Home: NextPage = () => {
     <div>
       <Head>
         <title>eStop Driving School</title>
-        <meta name="description" content="Teaching driving since 1983" />
+        <meta name="description" content="Teaching Driving to Teenagers, Adults and Seniors Since 1983" />
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
@@ -83,6 +83,9 @@ const Home: NextPage = () => {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff"></meta>
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Quicksand&display=swap');
+        </style>
       </Head>
 
       <main className="main">
