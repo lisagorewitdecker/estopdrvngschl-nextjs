efac5055c756dbb28c32c4d4c5dd098ef965f4b5
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2023-01-05 13:23:06 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -121,8 +121,8 @@ const Home: NextPage = () => {
         >
           <div className="container mx-auto">
             <div className="text-2xl mb-16 p-4" style={{ maxWidth: 480 }}>
-              <div className="message">
-                GIVE ME 6 HOURS Of YOUR TIME AND I WILL GIVE YOU 30,000 DAYS!</div>
+              <div class="message" style={{margin:0px; 0px; 40px; 0px !important, height: 250px;}}>
+                GIVE ME 6 HOURS Of YOUR TIME AND I WILL GIVE YOU 30,000 DAYS!
               <a
                 className="bg-red-500 hover:bg-red-800 text-white px-4 py-2 m-2 text-xl"
                 href="https://calendly.com/estop"
@@ -131,7 +131,7 @@ const Home: NextPage = () => {
               >
                 TRY NOW
               </a>
-              
+              </div>
             </div>
           </div>
         </div>
