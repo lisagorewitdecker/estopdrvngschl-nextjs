8d16bdd324a93b38e6c3fcbaa2e79813aa4ad0b1
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-05-20 15:28:08 -0700

    Delete public/googleanalytics.js

--- public/googleanalytics.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-DK82SLK3W9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-DK82SLK3W9');
-</script>
