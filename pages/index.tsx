4d6910e219d4c7a3ac19dd3f8301297317a9769b
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-08 12:55:19 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { useCallback } from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import type { NextPage } from "next";
+// import Head from "next/head";
+// import Image from "next/image";
+// import { useCallback } from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
+function MyDocument() {
+  return (
+    <Html>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GOOGLE_TAG_MANAGER_ID');
+            `,
+          }}
+        />
+      </Head>
+      <body> 
 const NavConfig = [
   {
     name: "HOME",
