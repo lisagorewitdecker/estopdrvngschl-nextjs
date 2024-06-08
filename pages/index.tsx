45f74cd08c3f009b4904c5cbd4b62cf8ad9440e2
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-08 14:09:39 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,14 +59,13 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <script dangerouslySetInnerHTML={{
-    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-M2RDL48P');`,
-  }}>
-  </script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VKSKK5CQ59"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-VKSKK5CQ59');
+</script>
         <title>eStop Driving School</title>
         <meta name="description" content="Teaching Driving to Teenagers, Adults, and Seniors Since 1983" />
         <link rel="icon" href="/favicon.ico" />
