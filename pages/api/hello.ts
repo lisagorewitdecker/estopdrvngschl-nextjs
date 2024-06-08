af1f18ac4b40497b739d318e07a57d486d578134
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-08 11:29:23 -0700

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import GoogleTagManager from '@magicul/next-google-tag-manager';
 
 type Data = {
   name: string
 }
-
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <GoogleTagManager id="GTM-HY6B7RQQ9N" />
+      {children}
+    </body>
+  </html>
+);
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
