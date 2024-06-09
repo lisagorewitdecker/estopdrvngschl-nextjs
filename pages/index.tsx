09f33d97e3bdf9a8073011ebef6ce029d3a15350
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-09 13:07:04 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,16 @@ import Head from "next/head";
 import Image from "next/image";
 import { useCallback } from "react";
 import { Analytics } from "@vercel/analytics/react";
-import TagManager from 'react-gtm-module'
+import { TagManager ] from "react-gtm-module";
 
-const tagManagerArgs = {
+const tagManagerArgs = [
+    {
     gtmId: "GTM-HY6B7RQQ9N"
-}
-TagManager.initialize(tagManagerArgs},
-];
+     },
+     {
+    TagManager.initialize(tagManagerArgs}
+    ),
+    ];
 
 const NavConfig = [
   {
