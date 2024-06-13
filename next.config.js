4b6502d4451bbb2515dada5d96a5cb463d731c6e
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-13 12:13:55 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+/** module.exports = { */
+ /** reactStrictMode: true, */
+/**} */
+
 module.exports = {
-  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
