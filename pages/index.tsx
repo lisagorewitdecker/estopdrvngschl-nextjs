40c941fe1fab75545b296a5d078f1b4103466a81
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-13 17:07:30 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { NextPage } from "next";
 import Head from "next/head";
-import body from "next/body";
+import Body from "next/body";
 import Image from "next/image";
 import { useCallback } from "react";
 
