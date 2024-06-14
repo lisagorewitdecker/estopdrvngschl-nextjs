74d4fd6aecc744258d89279069657a3d0fc3ba98
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-14 10:15:37 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -274,6 +274,7 @@ const Home: NextPage = () => {
           </button>
           </form>
 </div>
+              </div>
           </main>
       <footer 
         className="bg-gray-900 text-gray-400 py-4 text-sm">
