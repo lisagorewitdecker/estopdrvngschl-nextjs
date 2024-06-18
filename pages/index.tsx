896378fbf6b05be2e14cf332f8004d5b779c2cab
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-18 15:12:44 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Body from "@req-body/api";
+import Html from "@req-html/api";
 import { useCallback } from "react";
 
 const NavConfig = [
