b535f96803991c27e03fbe070402548167c4c1b8
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-18 15:41:26 -0700

    Update index.jsx

--- req-html/api/index.jsx
+++ req-html/api/index.jsx
@@ -1,4 +1,4 @@
 export default async function handler(req, res) {
   const { html } = req;
-  return res.send(`Hello ${html.name}, you just parsed the request html!`);
+  return res.send(`${html}`);
 }
