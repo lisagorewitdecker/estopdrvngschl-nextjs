e28471beaa86d49428437b47f537e4a3cf752bb7
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-18 16:37:45 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-
-library.add(
+library.add (
   faCoffee,
   faCheckSquare,
   faUser,
@@ -20,8 +19,26 @@ library.add(
   faUniversalAccess,
   faCar,
   faArrowUp
+  };
+ return (
+  <>
+    <Head>
+      <title>Your title</title>
+      <meta name="description" content="Your description" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Script id="gtm" strategy="afterInteractive">
+      {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+      `}
+    </Script>
+    <Component {...pageProps} />
+  </>
 );
- 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
