befc3d5e905252ae20419802eba034ca7a168feb
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-21 17:47:54 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import Script from 'next/script';
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/next";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { useCallback } from "react";
 
 const NavConfig = [
