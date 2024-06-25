27d21c80419de25c8115f2df7d8316d16e0a885d
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-25 12:11:04 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
-
 import {
   faArrowUp,
   faCar,
@@ -12,17 +11,15 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-
-library.add(
+(library.add
   faCoffee,
   faCheckSquare,
   faUser,
   faChild,
   faUniversalAccess,
   faCar,
-  faArrowUp
-)
-}
+  faArrowUp)
+}}
 export class MyApp extends Component {
     render() {
         return (
