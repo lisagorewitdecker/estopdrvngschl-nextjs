f7e5621232958a4c783224584d505257e6630dcc
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-26 17:44:01 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,8 +21,16 @@ library.add(
   faCar,
   faArrowUp
 );
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics mode={'production'} />;
+    </>
+  );
 }
+ 
 export default MyApp;
