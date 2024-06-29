8487113412825d249e4e185f48242433b0a3c7bd
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-29 15:49:49 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Script from 'next/script';
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { NextPage } from "next";
@@ -139,13 +138,16 @@ const Home: NextPage = () => {
           <h1 className="text-2xl text-center py-4 font-bold">eStop Driving School, Novato, CA &ndash; Driving Lessons in Marin County & Sonoma County!</h1>
           <div className="grid sm:grid-cols-1  md:grid-cols-2 gap-4">
             <div className="flex items-center justify-center">
+                <div className="container">
+                  <h3>Teshome (Tony or TK) WISHES FOR ALL TO BE WELL TODAY! HAVE A SAFE WEEKEND DRIVING!</h3>
+                  </div>
               <a 
                 href="https://qualitybusinessawards.com/2024/the-best-driving-school-in-novato/nile-driving-school?_uax=NjIxMjM6OTUyNDE3MDI6ODc2NDI6NDYwNzQ2OjI1NzUyMTk1MDM6NTYxMzY1MjE"
                 rel="noopener noreferrer"
                 >
               <Image
                 src="2024-best-award.jpg"
-                alt="Novato Best Driving Instructor Award"
+                alt="Novato-Marin County Best Driving Instructor Award"
                 width={350}
                 height={350}
               />
