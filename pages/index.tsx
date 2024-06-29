9244af7856811e8d4d374478791ac4235fd55818
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-29 16:51:37 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -140,7 +140,7 @@ const Home: NextPage = () => {
           <div className="grid sm:grid-cols-1  md:grid-cols-2 gap-4">
             <div className="flex items-center align-center">
             <h3 className="center-text-bold">TESHOME (TONY/TK) WISHES FOR ALL TO BE WELL TODAY!</h3>
-               </div>
+             
             <div className="flex items-center align-center">
                 <a 
                 href="https://qualitybusinessawards.com/2024/the-best-driving-school-in-novato/nile-driving-school?_uax=NjIxMjM6OTUyNDE3MDI6ODc2NDI6NDYwNzQ2OjI1NzUyMTk1MDM6NTYxMzY1MjE"
@@ -184,6 +184,8 @@ const Home: NextPage = () => {
               </p>
               </p> 
         </div>
+            </div>
+          </div>
         <div
           id="schedule"
           className="flex py-20"
