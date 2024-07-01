2941bcab9932c07b75fe9a6f51a3afd085070d85
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-06-30 19:41:12 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -162,8 +162,8 @@ const Home: NextPage = () => {
                 Welcome to eStop Driving School. I am Tony(Teshome), the founder and owner of eStop Driving School
                 (previously known as Nile Driving School). I have been teaching behind-the-wheel driving since 1983.
               </p>
-              <p className="mb-2"> I have trained more than 5,000 teenagers, adults, and seniors (of course, they are of legal driving age and status). 
-                Basic training and services are at a flat hourly rate of $115. If you need any additional services, please do not hesitate to contact me for more details.
+              <p className="mb-2"> I have trained more than 33,000 plus teenagers  and over 5,000 adults, and seniors during my almost four decade career (of course, they are of legal driving age and status). 
+                Basic training and services are at a flat hourly rate of <b>$115</b>. If you need any additional services, please do not hesitate to contact me for more details.
               </p>
               <p className="mb-2">
                  Since 1983, I Have 40 Years of Experience Training and Teaching Adults,
