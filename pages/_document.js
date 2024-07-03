882e4f68e5fd69124a68435394f2d1727ebff043
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-07-02 17:54:31 -0700

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import { useCountUp } from 'react-countup';
+
+const CompleteHook = () => {
+  const countUpRef = React.useRef(null);
+  const { start, pauseResume, reset, update } = useCountUp({
+    ref: countUpRef,
+    start: 0,
+    end: 1234567,
+    delay: 1000,
+    duration: 5,
+    onReset: () => console.log('Resetted!'),
+    onUpdate: () => console.log('Updated!'),
+    onPauseResume: () => console.log('Paused or resumed!'),
+    onStart: ({ pauseResume }) => console.log(pauseResume),
+    onEnd: ({ pauseResume }) => console.log(pauseResume),
+  });
+  return (
+    <div>
+      <div ref={countUpRef} />
+      <button onClick={start}>Start</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={pauseResume}>Pause/Resume</button>
+      <button onClick={() => update(2000)}>Update to 2000</button>
+    </div>
+  );
+};
