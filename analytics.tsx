508692e0ce3983da031f90b28731b1f84ac967f0
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-07-02 18:00:58 -0700

    Update analytics.tsx

--- analytics.tsx
+++ analytics.tsx
@@ -1,6 +1,6 @@
 import { headers } from 'next/headers'
 import Script from 'next/script'
-// import { EU_COUNTRY_CODES } from '@/lib/constants' // list of EU country codes in ISO 3166-1 format
+import { EU_COUNTRY_CODES } from '@/lib/constants' // list of EU country codes in ISO 3166-1 format
 
 export default function Analytics() {
   const countryCode = headers().get('x-vercel-ip-country') || 'US'
