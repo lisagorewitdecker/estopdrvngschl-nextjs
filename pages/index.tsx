bf196b325ed6878b2d081c1a136557ae46d1127a
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-07-15 11:29:43 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -141,7 +141,7 @@ const Home: NextPage = () => {
             <div className="flex items-center justify-center">
               <div className="container">
                 <h3 className="text-center">
-                  <b>TESHOME (TONY/TK) IS GRATEFUL FOR 26,388 DAYS ALIVE & EVERY BREATH I TAKE EVERYDAY!</b></h3>
+                  <b>TESHOME (TONY/TK) IS GRATEFUL FOR 26,340 DAYS ALIVE & EVERY BREATH I TAKE EVERYDAY!</b></h3>
               </div>
                
               <div
