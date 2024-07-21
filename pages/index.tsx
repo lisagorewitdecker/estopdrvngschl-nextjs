3f715429e1a1d09c4912fde18f0a5c29e0faacea
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-07-20 18:08:56 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,14 +66,10 @@ const Home: NextPage = () => {
   window.dataLayer = window.dataLayer || [];
   function gtag() { dataLayer.push(arguments); }
   gtag('consent', 'default', {
-    'ad_user_data': 'denied',
-    'ad_personalization': 'denied',
-    'ad_storage': 'denied',
-    'analytics_storage': 'denied',
-    'wait_for_update': 500,
   });
   gtag('js', new Date());
   gtag('config', 'Google tag G-B0JWV1HFBS');
+      }
   </script>
         <title>eStop Driving School, The Best Driving School of Novato in Marin County to Sonoma County, CA | Teaching Expert Driving in North Bay, South Bay — San Francisco | Instructor Has Over Four Decades of Experience</title>
         <meta name="description" content="Award Winning | Teaching Driving Teenagers, Adults, & Seniors How to Drive Marin County & Since 1983" />
