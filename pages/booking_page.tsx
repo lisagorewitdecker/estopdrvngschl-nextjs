394ae5b7ae9023d71ada58b5a9dba26ea275d553
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-07-29 14:25:11 -0700

    Create booking_page.tsx

---
+++ pages/booking_page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <iframe
+      src="https://www.example.com"
+      width="800"
+      height="600"
+      title="Example iframe"
+    />
+  );
+};
+
+export default MyComponent;
