8e57d1258c0f46c0b6b33a4aaa6094451d35ded8
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-07-29 15:14:47 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
+
+Script
+
+contentSecurityPolicy: {
+  directives: {
+    default-src: ["'self'"],
+    frame-src: ["https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ2X5prOOhhr9YY9lm18M6ccMEaEvW6SXqUkLSGyd5ZPeWZ_azMZEpImoszopFVTB4UXsnNkDOfy"],
+  // swMinify: true,
+  },
+}
+
 const nextConfig = {
   reactStrictMode: true,
   // swMinify: true,
