96d617d5c99ca8f25dbbc018af22de3d82490db8
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-07-31 17:24:29 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,8 +42,7 @@ const ServiceConfig = [
     icon: "user",
     title: "ADULTS",
     description: `Three 2-Hour Driving Lessons For Adults, Ages 18 and Up.
-    (The one-Hour Rate is $125 an Hour and Includes All Taxes and Fees). Total Service Cost is $750. 
-     <h3><a id="schedule" rel="noopener noreferrer"> SCHEDULE NOW!</a></h3>`
+    (The one-Hour Rate is $125 an Hour and Includes All Taxes and Fees). Total Service Cost is $750.`
   },
   {
     icon: "universal-access",
