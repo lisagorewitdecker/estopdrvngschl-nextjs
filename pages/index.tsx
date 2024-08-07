18b5789fcb7beed1a0bc75da3cb26f680a76dfac
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-08-07 14:58:14 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,14 +64,14 @@ const Home: NextPage = () => {
       <Head>
         <title>eStop Driving School, The Best Driving School of Novato in Marin County to Sonoma County, CA | Teaching Expert Driving in North Bay, South Bay — San Francisco | Instructor Has Over Four Decades of Experience</title>
         <meta name="description" content="Award Winning | Teaching Driving Teenagers, Adults, & Seniors How to Drive Marin County & Since 1983" />
-        <meta name="description" content"GTM-52S8PDBJ" />
-        <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-52S8PDBJ');</script>
-<!-- End Google Tag Manager -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-QG0BYSYEY2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QG0BYSYEY2');
+</script>
       
        </script>
         <link rel="icon" href="/favicon.ico" />
