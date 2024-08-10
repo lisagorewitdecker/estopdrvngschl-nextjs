3261a0dc1f14fb5443361ca358aa786f54ec6b8e
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-08-10 15:19:04 -0700

    Update layouts.js

--- components/layouts.js
+++ components/layouts.js
@@ -1,10 +1,16 @@
-import Navbar from './navbar';
-import Footer from './footer';
+import useSWR from 'swr'
+import Navbar from './navbar'
+import Footer from './footer'
  
 export default function Layout({ children }) {
+  const { data, error } = useSWR('/api/navigation', fetcher)
+ 
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+ 
   return (
     <>
-      <Navbar />
+      <Navbar links={data.links} />
       <main>{children}</main>
       <Footer />
     </>
