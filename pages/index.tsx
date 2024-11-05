8a60d8a9b4f8ed5bf5db452f8cd8efc1a143d0f3
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-11-05 12:50:21 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -312,13 +312,21 @@ const Home: NextPage = () => {
               </div>
             </div>
           </div>
+
+          
         </div>
-        </main>
-      <footer className="bg-gray-900 text-gray-400 py-4 text-sm">
-         <div className="locations">
+       </main>
+      </div>
+         <div 
+           className="locations">
          <h2>eStop Driving School Provides Dring Lessons to The Following Locations: Novato, CA; Tiburon, CA; Petaluma, CA; San Rafael, CA; Corte Madera, CA; Sausalito, CA; Sebastopol, CA; Mill Valley, CA; San Anselmo, CA; Greenbrae CA; Kentfield, CA. If You Do Not See Your Location Listed Here; 
            Please Do Not Hesitate to Contact Teshome (Tony or TK) Via Text or Phone.</h2> 
          </div>
+        <footer 
+          className="bg-gray-900 
+          text-gray-400 
+          py-4 t
+          text-sm">
         <div className="container m-auto text-center">
           Copyright @2024{" "} | 
           <a
