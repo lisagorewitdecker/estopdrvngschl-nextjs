ec376f4033fb57e6d76918d394d48fac2ed415ca
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2024-11-21 16:22:52 -0800

    Update index.tsx
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -277,7 +277,7 @@ Basic Training and Services are at an Hourly Rate of $125. If You Happen To Need
                SCHEDULE AN APPOINTMENT
              </h2>
           <p className="text-center  mx-2">
-              Once you have selected an appointment time and filled out the info. Keep your browser window open a new window and go to your email. You will receive a confirmation code from Google. Enter the confirmation code and your time will be set.
+              Once you have selected an appointment time and date, look into your email for a Google email with a confirmation code. Once you open your browser, copy and paste the confirmation code from your email into the confirmation input text line and hit submit, and you will be scheduled for the appointment. 
             </p>            
               <iframe
                 src="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ2ZCzKMpxsDgy3QIYZTfsvvJUdvRk-jvBV0w8lJruHsQC7XKkoXjSkAI2Tti5absT-wnaXc05Zy"
