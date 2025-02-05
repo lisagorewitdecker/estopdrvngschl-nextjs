fa696ea043287a6b0976f636e14e97d576f17eac
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2025-02-04 16:27:01 -0800

    Update next-env.d.ts
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,22 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export = content;
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_API_URL: string;
+    // Add other environment variables here
+  }
+}
