e0034d1017c79c74b9938c5eda78ccb6907f0b32
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2025-02-19 15:38:45 -0800

    Update index.tsx
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -272,6 +272,18 @@ const Home: NextPage = () => {
                     className="flex  */}
          py-5 
         bg-gray-200">
+                    <div 
+                        style="background:#dd524c;"
+                        id="schedule"
+                        className="py-3 
+                        grid xs:grid-cols-1  
+                        md:grid-cols-2 
+                        lg:grid-cols-3 
+                        gap-4"
+                        >
+                         <h2 className="text-2xl text-center py-3 font-bold">
+                             NEW SERVICES
+                         </h2>
                     <div id="schedule"
                          className="container mx-auto py-2">
                         <h2 className="text-2xl text-center py-3 font-bold">
