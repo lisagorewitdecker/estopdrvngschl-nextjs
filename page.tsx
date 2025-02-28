1647b345024dfbbd83fabfac55744bdabee0ec54
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2025-02-27 16:43:38 -0800

    Update page.tsx
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

--- page.tsx
+++ page.tsx
@@ -1,11 +1,21 @@
 'use client'
- 
-import { use } from 'react'
- 
+
+import { useEffect, useState } from "react";
+
 export default function Page({
   params,
 }: {
   params: Promise<{ slug: string }>
 }) {
-  const { slug } = use(params)
+  const [slug, setSlug] = useState<string | null>(null)
+
+  useEffect(() => {
+    params.then((data) => setSlug(data.slug))
+  }, [params])
+
+  if (!slug) {
+    return <div>Loading...</div>
+  }
+
+  return <div>Slug: {slug}</div>
 }
