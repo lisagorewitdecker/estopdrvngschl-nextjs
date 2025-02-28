590e45f6244fc21a8fe36a7109f6c1a46432b965
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2025-02-27 18:13:04 -0800

    Create about.tsx
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "About";
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to the About page.</p>
+        </div>
+    );
+};
+
+export default About;
